f59b15e727fa90188113d052da0f3abcf89aa9fd
conor-mccullough <103977699+conor-mccullough@users.noreply.github.com>
2023-03-21 16:12:05 +1100

    Update group_members_generate.sh

--- identity/group_members_generate.sh
+++ identity/group_members_generate.sh
@@ -3,6 +3,8 @@
 # Generates 520 Vault entities, then assigns them to a group
 # Used to test group exhaustion theory
 
+GROUPNAME=treehouse
+
 for i in {1..520}
 do
 	vault write -field=id identity/entity name=conor_clone_no_${i} policies=test
@@ -10,4 +12,7 @@ done
 
 ENTITIES=$(vault list identity/entity/id | sed 1,2d | tr '\n' ',' | sed 's/.$//')
 
-vault write identity/group name=treehouse member_entity_ids="$ENTITIES"
+vault write identity/group name=$GROUPNAME member_entity_ids="$ENTITIES"
+
+echo "------------------------------------"
+echo "Member count for $GROUP is $COUNT"
