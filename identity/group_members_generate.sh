71681c937ae80338a5b4b2641cf25e8c4ba47ff7
Conor McCullough <103977699+conor-mccullough@users.noreply.github.com>
2023-05-29 13:43:49 +1000

    Update group_members_generate.sh
    added to-do for script rerun issue.

--- identity/group_members_generate.sh
+++ identity/group_members_generate.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Currently this script only works properly the first time it's run, as each rerun will simply iterate through the previously created `conor_clone_no_<number>` entities.
+# Need to either create a prompt asking to define a name pattern and store it as a variable or write a short random string generator when I get the time.
+
 # Generates 520 Vault entities, then assigns them to a group
 # Used to test group exhaustion theory
 
 vault auth enable userpass
+
 
 GROUPNAME=treehouse
 for i in {1..520}
